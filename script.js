7987487597744943177c0a4435ad5cc6ebd65b23
SteakTheStake <gabecorr25@gmail.com>
2023-10-07 22:51:59 -0700

    changes

--- script.js
+++ script.js
@@ -1,64 +1,43 @@
-const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor");
+var sound = new Audio("content/sounds/click.ogg");
 
-const textArray = ["Imagination", "Possibilities", "Building", "Textures"];
-const typingDelay = 200;
-const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
-let textArrayIndex = 0;
-let charIndex = 0;
-
-function type() {
-  if (charIndex < textArray[textArrayIndex].length) {
-    if (!cursorSpan.classList.contains("typing"))
-      cursorSpan.classList.add("typing");
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  } else {
-    cursorSpan.classList.remove("typing");
-    setTimeout(erase, newTextDelay);
-  }
+function setupClickEvent(elementId, callback) {
+  document.getElementById(elementId).addEventListener("click", function () {
+    sound.play();
+    if (callback) {
+      setTimeout(callback, 250);
+    }
+  });
 }
 
-function erase() {
-  if (charIndex > 0) {
-    if (!cursorSpan.classList.contains("typing"))
-      cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(
-      0,
-      charIndex - 1
-    );
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-    setTimeout(type, typingDelay + 1100);
-  }
+function singleplayer() {
+  document.location.href = 'singleplayer.html';
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  // On DOM Load initiate the effect
-  if (textArray.length) setTimeout(type, newTextDelay + 250);
-});
+function options() {
+  document.location.href = '404.html';
+}
+
+function quit() {
+  document.location.href = 'home.html';
+}
+
+function title() {
+  document.location.href = 'home.html';
+}
+
+function startMultiplayer() {
+  window.location.href = "patreon-auth.html";
+}
+
+setupClickEvent("click7");
+setupClickEvent("click4", quit);
+setupClickEvent("click1", singleplayer);
+setupClickEvent("click2", startMultiplayer);
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
 
 
-
-const mountainLeft = document.querySelector('#mountain_left');
-const mountainRight = document.querySelector('#mountain_right');
-const cloud1 = document.querySelector('#clouds_1');
-const cloud2 = document.querySelector('#clouds_2');
-const text = document.querySelector('#text');
-const man = document.querySelector('#man');
-
-window.addEventListener('scroll',()=>{
-  let value = scrollY;
-  mountainLeft.style.left = `-${value/0.7}px`
-  cloud2.style.left = `-${value*2}px`
-  mountainRight.style.left = `${value/0.7}px`
-  cloud1.style.left = `${value*2}px`
-  text.style.bottom = `-${value}px`;
-  man.style.height = `${window.innerHeight - value}px`
+new Crate({
+  server: '1159871598644445266', // SummitMC
+  channel: '1159878139569258517' // #⛺┃general
 })
