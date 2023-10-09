09ab90904320e91223d99240b4b6cce9ea3925ff
SteakTheStake <gabecorr25@gmail.com>
2023-10-09 15:34:44 -0700

    changes

--- pages/js/patreon-auth.js
+++ pages/js/patreon-auth.js
@@ -1,12 +1,14 @@
-fetch('/check-patron-status')
-  .then(response => response.json())
-  .then(data => {
-    if (data.isPatron) {
-      window.location.href = "/pages/vault.html";
-    } else {
-      window.location.href = "/pages/not-patron.html";
-    }
-  })
-  .catch(error => {
-    console.error("Error checking patron status:", error);
-  });
+function startPatreonAuth() {
+  fetch('/check-patron-status')
+    .then(response => response.json())
+    .then(data => {
+      if (data.isPatron) {
+        window.location.href = "/pages/vault.html";
+      } else {
+        window.location.href = "/pages/not-patron.html";
+      }
+    })
+    .catch(error => {
+      console.error("Error checking patron status:", error);
+    });
+}
