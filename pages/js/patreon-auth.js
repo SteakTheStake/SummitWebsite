fb3889afa6bd5c19485733ef30c0d8faa6e2ee3f
SteakTheStake <gabecorr25@gmail.com>
2023-10-10 10:00:55 -0700

    changes

--- pages/js/patreon-auth.js
+++ pages/js/patreon-auth.js
@@ -1,6 +1,12 @@
 function startPatreonAuth() {
   fetch('/check-patron-status')
-    .then(response => response.json())
+    .then(response => {
+      // Ensure the response is valid JSON
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
     .then(data => {
       if (data.isPatron) {
         window.location.href = "/pages/vault.html";
@@ -11,8 +17,10 @@ function startPatreonAuth() {
     .catch(error => {
       console.error("Error checking patron status:", error);
     });
-  fetch('/status')
-    .then(response => response.text())
-    .then(text => console.log(text))
-    .catch(error => console.error('Error:', error));
 }
+
+// You can call this function on a button click or any other event
+// For instance, if you have a button with an ID 'patreonAuthBtn', you can do:
+document.querySelectorAll('.patreonAuthBtnClass').forEach(element => {
+  element.addEventListener('click', startPatreonAuth);
+});
